92faaaf3651e80bb1ee896b35e221b00151d2c33
cnrzhang <chz9577@nyu.edu>
2025-03-05 21:54:03 -0500

    colorsort

--- Week1/colorsort.cpp
+++ Week1/colorsort.cpp
@@ -7,12 +7,13 @@ class Solution {
         void sortColors(vector<int>& nums) {
             int n = nums.size();
             int low = 0, high = n - 1, mid = 0;
-            while( high >= mid)
-                if(nums[mid] == 0) // case 1, we leave in place and proceed to next 
+            while( high >= mid){
+                if(nums[mid] == 0){ // case 1, we leave in place and proceed to next 
                     swap(nums[low], nums[mid]);
                     mid++;
                     low++;
-                    else if(nums[mid] == 1){ //case 2, we increase by 1 but leave low behind
+                }
+                else if(nums[mid] == 1){ //case 2, we increase by 1 but leave low behind
                         mid++;
                     }
                     else{//case 3, we move it to the end of the array, then decrement high by one
@@ -20,5 +21,6 @@ class Solution {
                         high--;
                     }
             
+        }
         }
     };
